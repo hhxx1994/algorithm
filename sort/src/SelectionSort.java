6cf8467a19c47677e0223976206f37ac8828da6c
huanghaoxing <huanghaoxing@youzan.com>
2020-07-28 07:18:52 +0800

    选择排序

---
+++ sort/src/SelectionSort.java
@@ -0,0 +1,37 @@
+/**
+ * @author huanghaoxing
+ */
+public class SelectionSort {
+
+  public static void sort(int[] arr) {
+    if (arr == null || arr.length == 0) {
+      return;
+    }
+    int length = arr.length;
+    int minIndex;
+    for (int i = 0; i < length; i++) {
+      minIndex = i;
+      for (int j = i; j < length; j++) {
+        if (arr[j] < arr[i]) {
+          minIndex = j;
+        }
+      }
+      swap(arr, i, minIndex);
+    }
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {2, 3, 1, 5, 7, 3, 7, 8};
+    sort(arr);
+    for (int i : arr) {
+      System.out.print(i);
+      System.out.print(",");
+    }
+  }
+}
