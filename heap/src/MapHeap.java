337cbc7f0771eb8b8f1b3b3235b9b655da9ac4d0
huanghaoxing <huanghaoxing@youzan.com>
2020-08-17 08:54:12 +0800

    堆

---
+++ heap/src/MapHeap.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+/**
+ * @author huanghaoxing
+ */
+public class MapHeap<E extends Comparable> {
+  ArrayList<E> array;
+
+  public MapHeap(int capacity) {
+    array = new ArrayList<>(capacity);
+  }
+
+  private int leftChild(int index) {
+    return index * 2 + 1;
+  }
+
+  private int rightChild(int index) {
+    return index * 2 + 2;
+  }
+
+  private int parent(int index) {
+    return (index - 1) / 2;
+  }
+
+  public void add(E e) {
+    array.add(e);
+    siftUp(array.size() - 1);
+  }
+
+  public E extractMax() {
+    swap(0, array.size() - 1);
+    E max = array.remove(array.size() - 1);
+    siftDown(0);
+    return max;
+  }
+
+  private void siftDown(int index) {
+    while (leftChild(index) < array.size()) {
+      int i = leftChild(index);
+      if (i + 1 < array.size() && array.get(i + 1).compareTo(array.get(i)) > 0) {
+        i++;
+      }
+      if (array.get(i).compareTo(array.get(index)) <= 0) {
+        break;
+      }
+      swap(i, index);
+      index = i;
+    }
+  }
+
+  private void siftUp(int index) {
+    while (parent(index) >= 0 && array.get(parent(index)).compareTo(array.get(index)) < 0) {
+      swap(parent(index), index);
+      index = parent(index);
+    }
+  }
+
+  private void swap(int parent, int index) {
+    E temp = array.get(parent);
+    array.set(parent, array.get(index));
+    array.set(index, temp);
+  }
+
+  public static void main(String[] args) {
+    MapHeap mapHeap = new MapHeap(10);
+    for (int i = 0; i < 10; i++)
+      mapHeap.add(i);
+
+    for (int i = 0; i < 10; i++)
+      System.out.println(mapHeap.extractMax());
+
+  }
+
+}
