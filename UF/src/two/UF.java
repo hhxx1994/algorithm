579e300c3df1106a591a43931095fc42cc1d7429
huanghaoxing13696 <huanghaoxing696@hellobike.com>
2021-01-03 21:17:30 +0800

    并查集

---
+++ UF/src/two/UF.java
@@ -0,0 +1,45 @@
+package two;
+
+/**
+ * @author huanghaoxing
+ */
+public class UF {
+    int[] parent;
+    int[] rank;
+
+    public UF(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public void unionElement(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[qRoot] < rank[pRoot]) {
+            parent[qRoot] = pRoot;
+        } else {
+            parent[qRoot] = pRoot;
+            rank[pRoot] += 1;
+        }
+    }
+
+    private int find(int p) {
+        while (parent[p] != p) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+}
