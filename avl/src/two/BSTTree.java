06e57044687d9390a20b796c6c85f7fb2ee815c3
huanghaoxing13696 <huanghaoxing696@hellobike.com>
2021-01-09 11:46:37 +0800

    avl

--- avl/src/two/BSTTree.java
+++ avl/src/two/BSTTree.java
@@ -30,9 +30,6 @@ public class BSTTree {
         return node.height;
     }
 
-    private void setHeight(int height, Node node) {
-        node.height = height;
-    }
 
     private Node add(int data, Node node) {
         if (node == null) {
@@ -44,17 +41,65 @@ public class BSTTree {
         } else if (node.data < data) {
             node.right = add(data, node.right);
         }
-        int leftHeight = getHeight(node.left);
-        int rightHeight = getHeight(node.right);
-        setHeight(Math.max(leftHeight, rightHeight) + 1, node);
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
 
-        if (Math.abs(leftHeight - rightHeight) > 1) {
-            // unbalance
-            System.out.println("unbalance");
+        // LL
+        if (getBalanceFactor(node) > 1 && getBalanceFactor(node.left) >= 0) {
+            return rightRotate(node);
+        }
+
+        // RR
+        if (getBalanceFactor(node) < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+
+        // LR
+        if (getBalanceFactor(node) > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // RL
+        if (getBalanceFactor(node) < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
 
         return node;
     }
+
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        x.right = y;
+        y.left = T3;
+
+        y.height = Math.max(getHeight(y.left), getBalanceFactor(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
+    }
+
+    private Node leftRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.left;
+
+        x.left = y;
+        y.right = T3;
+
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    private int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
 
     public static void main(String[] args) {
         BSTTree bstTree = new BSTTree();
