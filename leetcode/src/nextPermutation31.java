afd6841a945a47036486a8fdb0f909663489e9f4
huanghaoxing13696 <huanghaoxing696@hellobike.com>
2022-03-12 20:04:44 +0800

    leetcode

---
+++ leetcode/src/nextPermutation31.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * @author huanghaoxing
+ */
+public class nextPermutation31 {
+
+    public void nextPermutation(int[] nums) {
+        int j = -1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                j = i - 1;
+                break;
+            }
+        }
+        if (j != -1) {
+            int k = -1;
+            for (int i = j + 1; i < nums.length; i++) {
+                if (nums[i] > nums[j]) {
+                    k = i;
+                }
+            }
+            if (k != -1) {
+                int temp = nums[k];
+                nums[k] = nums[j];
+                nums[j] = temp;
+                Arrays.sort(nums, j + 1, nums.length);
+            } else {
+                Arrays.sort(nums);
+            }
+        } else {
+            Arrays.sort(nums);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {1, 3, 2};
+        new nextPermutation31().nextPermutation(ints);
+    }
+
+}
---
+++ leetcode/src/reverseBetween92.java
@@ -0,0 +1,24 @@
+/**
+ * @author huanghaoxing
+ */
+public class reverseBetween92 {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        for (int i = 0; i < right - left; i++) {
+            ListNode next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummy.next;
+    }
+
+
+}
