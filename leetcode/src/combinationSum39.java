ae48381eb2fb93b0bd4a6db6ec205ceab434e1a9
huanghaoxing13696 <huanghaoxing696@hellobike.com>
2022-03-17 15:07:19 +0800

    dp

---
+++ leetcode/src/combinationSum39.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * @author huanghaoxing
+ */
+public class combinationSum39 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
--- leetcode/src/findTargetSumWays494.java
+++ leetcode/src/findTargetSumWays494.java
@@ -4,17 +4,34 @@
 public class findTargetSumWays494 {
 
     public int findTargetSumWays(int[] nums, int target) {
-
-        int[][] dp = new int[nums.length + 1][target + 1];
-        // dp[0][...] = 0;
-        // dp[...][0]
-        for (int j = 0; j <= target; j++) {
-            for (int i = 1; i <= nums.length; i++) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((target + sum) % 2 != 0) {
+            return 0;
+        }
+        int s = (target + sum) / 2;
+        if (s < 0) {
+            s = -s;
+        }
+        int[][] dp = new int[nums.length + 1][s + 1];
+        // dp[0][...] = 0
+        // dp[...][0] = 1
+        for (int i = 0; i <= nums.length; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            // 注意要从0开始
+            for (int j = 0; j <= s; j++) {
                 if (j - nums[i - 1] >= 0) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j + nums[i - 1]];
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[nums.length][target];
+
+        return dp[nums.length][s];
     }
 }
