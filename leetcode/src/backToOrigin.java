ac24b990ff56cbcbb9a8bf3de6c1da9953421f64
huanghaoxing13696 <huanghaoxing696@hellobike.com>
2022-03-18 14:38:59 +0800

    切割木头

---
+++ leetcode/src/backToOrigin.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author huanghaoxing
+ */
+public class backToOrigin {
+
+
+    // 备忘录
+    Map<String, Integer> memo = new HashMap<>();
+
+    public int backToOrigin(int length, int n) {
+        return backToOrigin(0, n, length);
+    }
+
+//    // dp[i][j] = dp[(i + 1) % length][j-1] + dp[(index - 1 + length) % length][j-1]
+//    // dp[0][n]
+//
+//    public void dp(int n, int length) {
+//
+//        int[][] dp = new int[length][n + 1];
+//
+//
+//    }
+
+    public int backToOrigin(int index, int n, int length) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0 && index == 0) {
+            return 1;
+        }
+        String key = index + "_" + n;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int ret = 0;
+        // 往前
+        ret += backToOrigin((index + 1) % length, n - 1, length);
+        // 往后
+        ret += backToOrigin((index - 1 + length) % length, n - 1, length);
+
+        memo.put(key, ret);
+        return ret;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new backToOrigin().backToOrigin(10, 2));
+    }
+}
